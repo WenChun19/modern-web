781a838551d0f175bba0769d4820b69b500207ff
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-12 15:40:51 +0800

    Business and Stats sections

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6
+    bg-blue-gradient font-poppins font-medium
+    text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
