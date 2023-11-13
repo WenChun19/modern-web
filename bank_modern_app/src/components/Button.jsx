3bc4165bb5376b6947b7062151694c43fd1e158d
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-13 20:51:29 +0800

    Billing and card deal section

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
       type="button"
       className={`py-4 px-6
     bg-blue-gradient font-poppins font-medium
-    text-[18px] text-primary outline-none ${styles}`}
+    text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,5 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          This is also amazing thing
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
